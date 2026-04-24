e940b2731a33c4ecb6f08ee85c17e79edf24d727
Swastik Jha <swastik.jha@finapsys.co.in>
2026-04-24 14:45:51 +0530

    new vercel origin added

--- src/shared/config/cors.js
+++ src/shared/config/cors.js
@@ -1,5 +1,9 @@
 export const corsOptions = {
-  origin: ["http://localhost:3000","https://swastikjha.com"],
+  origin: [
+    "http://localhost:3000",
+    "https://swastikjha.com",
+    "https://swastik-portfolio-eight.vercel.app/",
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
